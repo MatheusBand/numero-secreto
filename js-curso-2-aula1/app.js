5f2ce12b738e45084fb1a694926363a2138e9106
matheus bandeira <matheus.bandeiracosta@gmail.com>
2025-01-28 20:11:54 -0300

    diminuição da dificuldade

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,5 +1,5 @@
 let listaNumSorteados = [];// listas começa pelo indice 0//
-let numLimte = 10;
+let numLimte = 50;
 let numSecreto = numAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTexto(tag, Texto){
 
 function mensagemInicial() {
     exibirTexto('h1', 'Jogo do Número Secreto');
-exibirTexto('p', 'Escolha um número entre 1 e 10');
+exibirTexto('p', 'Escolha um número entre 1 e 50');
 }
 
 mensagemInicial()
